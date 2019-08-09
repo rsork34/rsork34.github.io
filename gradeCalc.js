d9360a3ec89076bf882177f6c4a5266cb076f575
rsork34 <37781157+rsork34@users.noreply.github.com>
2019-08-09 11:06:04 -0400

    Add files via upload
    Improved validation, add new rows dynamically, bootstrap styling

--- gradeCalc.js
+++ gradeCalc.js
@@ -1,13 +1,7 @@
-const NUM_ROWS = 10;
+const NUM_ROWS = 5;
+let curRows = 0;
 
-class grade {
-    constructor(mark, weight) {
-        this.mark = mark;
-        this.weight = weight;
-    }
-}
-
-$(document).ready(function () { 
+$(document).ready(function () {
     loadTable();
 });
 
@@ -20,61 +14,106 @@ function loadTable() {
 
     // Load table rows
     for (let i = 0; i < NUM_ROWS; i++) {
-        tableHtmlString += `<tr><td>${i + 1}</td>`;
-        tableHtmlString += `<td><input type="number" placeholder="0.0"></td>`;
-        tableHtmlString += `<td><input type="number" placeholder="0.0"></td></tr>`;
+        tableHtmlString += `<tr><td style="text-align:center;"><b>${i + 1}</b></td>`;
+        tableHtmlString += `<td><input id="gradeInput${i}" type="number" min="0" placeholder="0.0"></td>`;
+        tableHtmlString += `<td><input id="weightInput${i}" type="number" min="0" placeholder="0.0"></td></tr>`;
     }
 
     $('#gradesInputTable').append(tableHtmlString);
+    curRows = NUM_ROWS;
+}
+
+function validateRow(mark, weight, line) {
+    /* Ensure both fields are entered per row */
+    // Both fields are empty or weight 0 so grade does not factor in - skip row
+    if ((mark.length === 0 && weight.length === 0) || (weight === 0)) {
+        return 'skip';
+    }
+    else if (mark.length === 0 && weight.length !== 0) {
+        alert('Missing mark on line ' + line);
+        return 'invalid';
+    }
+    else if (mark.length !== 0 && weight.length === 0) {
+        alert('Missing weight on line ' + line);
+        return 'invalid';
+    }
+
+    // Validate input
+    if (mark < 0) {
+        alert('Invalid Mark On Line ' + line);
+        return 'invalid';
+    }
+    else if (weight < 0) {
+        alert('Invalid Weight On Line ' + line);
+        return 'invalid';
+    }
+
+    return 'valid';
 }
 
 function calculateGrade() {
-    let gradeArray = [];
-    let rows = $('#gradesInputTable > tbody > tr');
     let totalWeight = 0;
     let weightedGrade = 0;
 
-    console.log(rows);
+    // Minus one to ignore the header row
+    let numRows = $('#gradesInputTable tr').length - 1;
 
-    return;
+    for (let i = 0; i < numRows; i++) {
+        // Mark and weight for current row
+        let curMark = $(`#gradeInput${i}`).val();
+        let curWeight = $(`#weightInput${i}`).val();
 
-    // Add each row of grade/weight to array of grades
-    for (let i = 0, j = 0; i < inputFields.length; i+= 2, j++) {
-        let tempGrade = new grade(inputFields[i].value, inputFields[i + 1].value / 100);
+        // Validate input
+        // Skip row if weight is 0
+        let validInput = validateRow(curMark, curWeight, (i + 1));
+        if (validInput === 'skip') {
+            continue;
+        }
+        else if (validInput === 'invalid') {
+            return;
+        }
+
+        curMark = parseFloat(curMark);
+        curWeight = parseFloat(curWeight);
 
         // Don't count grades with no weight
-        if (tempGrade.weight <= 0) {
+        if (curWeight === 0) {
             continue;
         }
 
-        weightedGrade += parseFloat(tempGrade.weight) * parseFloat(tempGrade.mark);
-        totalWeight += parseFloat(tempGrade.weight);
-        gradeArray.push(tempGrade);
+        // Used for final calculation
+        weightedGrade += (curWeight * curMark);
+        totalWeight += curWeight;
+    }
+
+    // Invalid input
+    if (isNaN(totalWeight) || totalWeight === 0) {
+        alert('Please Enter Valid Weight(s)');
+        return;
+    }
+    // Invalid weight entered
+    else if (totalWeight > 100) {
+        alert('Total weight of grades must be 100. You entered: ' + totalWeight);
+        return;
     }
 
     // Calculate final grade
     let finalGrade = weightedGrade / totalWeight;
-    
-    // Invalid input
-    if (isNaN(totalWeight) || totalWeight == 0) {
-        document.getElementById("p1error").innerHTML = "Please Enter Valid Weight(s)";
-        return;
-    }
-    // Invalid weight entered
-    else if (totalWeight > 1 || totalWeight < 0) {
-        document.getElementById("p1error").innerHTML = "Total weight of grades is too high";
-        return;
-    }
-
-    document.getElementById("p1error").innerHTML = "Grade: " + finalGrade.toPrecision(4);
-
+    alert('Final Grade: ' + finalGrade);
 }
 
 function resetInput() {
-    let inputFields = document.getElementById("inputForm");
+    loadTable();
+}
 
-    for (i = 0; i < inputFields.length; i++) {
-        inputFields[i].value = 0;
-        inputFields[i].weight = 0;
-    }
+function addRow() {
+    let newRowHtml = '';
+
+    curRows++;
+
+    newRowHtml += `<tr><td style="text-align:center;"><b>${curRows}</b></td>`;
+    newRowHtml += `<td><input id="gradeInput${curRows}" type="number" min="0" placeholder="0.0"></td>`;
+    newRowHtml += `<td><input id="weightInput${curRows}" type="number" min="0" placeholder="0.0"></td></tr>`;
+
+    $('#gradesInputTable').append(newRowHtml);
 }
